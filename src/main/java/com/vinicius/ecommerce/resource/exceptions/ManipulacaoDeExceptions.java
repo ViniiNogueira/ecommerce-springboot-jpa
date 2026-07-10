e80b5e204819dd65f0f690165f4f28845ea08a60
Vinicius <viniciusnogueira2507@gmail.com>
2026-07-10 12:41:32 -0300

    Tratando exceções no Delete

--- src/main/java/com/vinicius/ecommerce/resource/exceptions/ManipulacaoDeExceptions.java
+++ src/main/java/com/vinicius/ecommerce/resource/exceptions/ManipulacaoDeExceptions.java
@@ -1,6 +1,7 @@
 package com.vinicius.ecommerce.resource.exceptions;
 
 import com.sun.net.httpserver.HttpsServer;
+import com.vinicius.ecommerce.services.exceptions.DatabaseException;
 import com.vinicius.ecommerce.services.exceptions.RecursoNaoEncontradoException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,15 @@ public class ManipulacaoDeExceptions {
     public ResponseEntity<ErroPadrao> recursoNaoEncontrado(RecursoNaoEncontradoException e,  HttpServletRequest request) {
         String error = "Recurso nao encontrado";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        ErroPadrao ep =  new ErroPadrao(Instant.now(),status.value() ,error ,e.getMessage(),request.getRequestURI());
+
+        return ResponseEntity.status(status).body(ep);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<ErroPadrao> database(DatabaseException e,  HttpServletRequest request) {
+        String error = "Erro no banco de dados";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         ErroPadrao ep =  new ErroPadrao(Instant.now(),status.value() ,error ,e.getMessage(),request.getRequestURI());
 
         return ResponseEntity.status(status).body(ep);
--- src/main/java/com/vinicius/ecommerce/services/UserService.java
+++ src/main/java/com/vinicius/ecommerce/services/UserService.java
@@ -2,8 +2,11 @@ package com.vinicius.ecommerce.services;
 
 import com.vinicius.ecommerce.entities.User;
 import com.vinicius.ecommerce.repositories.UserRepository;
+import com.vinicius.ecommerce.services.exceptions.DatabaseException;
 import com.vinicius.ecommerce.services.exceptions.RecursoNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RecursoNaoEncontradoException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
